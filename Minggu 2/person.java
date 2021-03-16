e4e5624060a9f2b06475104a10162fe6adf2b857
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-03-16 11:05:37 +0700

    Create person.java

---
+++ Minggu 2/person.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Tanti
+ */
+public class person {
+    String name;
+    int age;
+    
+    public person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age =age;
+    }
+}
