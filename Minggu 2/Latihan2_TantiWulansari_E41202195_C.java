a810944bf11486dfb966c4e57ece18fa379c43e2
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-03-16 11:03:04 +0700

    Create Latihan2_TantiWulansari_E41202195_C.java

---
+++ Minggu 2/Latihan2_TantiWulansari_E41202195_C.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2_tantiwulansari_e41202195_c;
+
+/**
+ *
+ * @author Tanti
+ */
+public class Latihan2_TantiWulansari_E41202195_C {
+
+    public static void main(String[] args) {
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+    
+        System.out.println (nama);
+        System.out.println (a);
+    }   
+}
