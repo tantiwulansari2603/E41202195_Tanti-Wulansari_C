fcc2b101fbf5fdea1f558dbd654a9ac3e578d140
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-03-16 11:07:03 +0700

    Create TugasC_TantiWulansari_E41202195_C.java

---
+++ Minggu 2/TugasC_TantiWulansari_E41202195_C.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasc_tantiwulansari_e41202195_c;
+
+/**
+ *
+ * @author Tanti
+ */
+public class TugasC_TantiWulansari_E41202195_C {
+
+    public static void main(String[] args) {
+    boolean gameOver = false;
+    int students=50, classes=3;
+    double sales_tax;
+    short Number1;
+    
+    int 2beOrNot2be;
+    /**Didalam persyaratan untuk pendeklarasian variable tidak diperbolehkan/tidak 
+    diperkenankan untuk mengawali nama variabele dengan karakter numerik*/
+    
+    float price index;
+    /**Penndeklarasian variabel diatas seharusnya tidak boleh menggunakan spasi 
+    karena dalam beberapa persyaratan untuk pendeklarasian variabel tidak boleh ada spasi*/
+    
+    double lastYear'sPrice
+    /**Pendeklarasian double diatas salah karena dalam persyaratan penulisan variabel,
+    nama variabel tidak boleh terdiri dari karakter simbol kecuali tanda $ dan juga underscore (_)*/
+            
+    long class;
+    /**Nama variabel tidak boleh menggunakan kata kunci yang sudah ada didalam java
+    contoh while, break, class dan masih banyak lagi*/
+    }
+    
+}
