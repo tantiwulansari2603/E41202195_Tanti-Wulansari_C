ddd710db14300029fd868a38f629c7e28897d370
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-03-23 12:37:56 +0700

    Create TugasA_ArraySatuDimensi.java

---
+++ Minggu 3/TugasA_ArraySatuDimensi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasa_arraysatudimensi;
+
+/**
+ *
+ * @author Tanti
+ */
+public class TugasA_ArraySatuDimensi {
+
+    public static void main(String[] args) {
+        String[] nama = {"Tanti", "Wulansari"};
+       
+            for(int i=0; i<nama.length; i++) {
+                System.out.print(nama[i]);
+                System.out.print(" ");
+            }
+    }
+}
