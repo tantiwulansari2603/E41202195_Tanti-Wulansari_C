f5f6b2459836b50115c18c87fa7084c0ecffa290
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-03-23 12:31:45 +0700

    Create Latihan1_Array.java

---
+++ Minggu 3/Latihan1_Array.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1_array;
+
+/**
+ *
+ * @author Tanti
+ */
+public class Latihan1_Array {
+
+    public static void main(String[] args) {
+       int[] angka = {3, 7, 8, 1, 10, 20, 35};
+       
+      System.out.println("Jumlah Elemen Array Angka = "+angka.length);
+    }
+}
