3a0d5bd72f93505fed572439c4edd65e71b8ceb7
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-03-29 21:45:46 +0700

    Create Latihan3_StatementFor.java

---
+++ Minggu 4/Latihan3_StatementFor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanweek4;
+
+/**
+ *
+ * @author Tanti
+ */
+public class Latihan3_StatementFor {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
