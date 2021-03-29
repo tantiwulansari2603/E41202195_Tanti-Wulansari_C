1623d56a3cde0c73542bcdb3c41c93669a8b7228
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-03-29 21:45:53 +0700

    Create Latihan4_StatementForEach.java

---
+++ Minggu 4/Latihan4_StatementForEach.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanweek4;
+
+/**
+ *
+ * @author Tanti
+ */
+public class Latihan4_StatementForEach {
+    public static void main(String[] args) {
+        int angka[] = {3, 2, 42, 24, 12};
+        
+            for (int x : angka) {
+                System.out.print(x + " ");
+            }
+    }
+}
