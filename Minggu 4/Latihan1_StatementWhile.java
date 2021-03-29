7bef3b301de81c5696d6a4b8fe516469e82169fb
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-03-29 21:45:04 +0700

    Create Latihan1_StatementWhile.java

---
+++ Minggu 4/Latihan1_StatementWhile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanweek4;
+
+/**
+ *
+ * @author Tanti
+ */
+public class Latihan1_StatementWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i <= 10) {
+            System.out.println("Perulangan Ke-1 " + i);
+            i++;
+        }
+    }
+}
