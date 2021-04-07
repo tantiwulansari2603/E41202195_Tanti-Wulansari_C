b2f3f9f584c5f157f37d39b00ee79e8e28feeda1
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-07 09:34:56 +0700

    Create Node.java

---
+++ Minggu 5/Node.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Tanti
+ */
+public class Node {
+    public int id;
+    public String data;
+    public Node leftChild;
+    public Node rightChild;
+    
+        public void displayNode() {
+            System.out.println("{ " + id + ", " + data + " } ");
+        }
+}
