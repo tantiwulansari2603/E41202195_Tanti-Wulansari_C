4eb2cd0a8e805b8d13fc76d24daed7cf433c7972
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:56:09 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan_Week7/Abstract/Demo.java
@@ -0,0 +1,15 @@
+package Abstract;
+
+/**
+ *
+ * @author Tanti
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.method1();
+        ob.method2();
+    }
+    
+}
