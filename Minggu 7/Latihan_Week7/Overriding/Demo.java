4bdac165ff7864e5e15356439335b32dbe01e9c8
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:57:25 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan_Week7/Overriding/Demo.java
@@ -0,0 +1,14 @@
+package Overriding;
+
+/**
+ *
+ * @author Tanti
+ */
+public class Demo {
+
+   public static void main(String[] args) {
+        B subOb = new B();
+        subOb.TampilKeLayar();
+    }
+    
+}
