b3dd1df63620fb0db614a74d74fd0a3c780b3f2f
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:57:54 +0700

    Create A.java

---
+++ Minggu 7/Latihan_Week7/Pewarisan/A.java
@@ -0,0 +1,14 @@
+package Pewarisan;
+
+/**
+ *
+ * @author Tanti
+ */
+public class A {
+    int x;
+    int y;
+    
+    void TampilkanNilaiXY(){
+        System.out.println("Nilai X = " + x +", y = " + y);
+    } 
+}
