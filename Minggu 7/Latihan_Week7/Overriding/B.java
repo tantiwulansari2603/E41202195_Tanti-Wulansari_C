8df9387737ab76487ac184821fa97d2717c07613
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:56:57 +0700

    Create B.java

---
+++ Minggu 7/Latihan_Week7/Overriding/B.java
@@ -0,0 +1,13 @@
+package Overriding;
+
+/**
+ *
+ * @author Tanti
+ */
+public class B extends A {
+    public void TampilKeLayar() {
+        super.TampilKeLayar();
+        System.out.println(" Method B dipanggil");
+    }
+}
+
