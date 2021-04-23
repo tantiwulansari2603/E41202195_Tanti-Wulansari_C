dc67d41402c2ffb84300ee265dd44f8af1ca14e5
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:58:41 +0700

    Create Test.java

---
+++ Minggu 7/Latihan_Week7/Pewarisan2/Test.java
@@ -0,0 +1,15 @@
+package Pewarisan2;
+
+/**
+ *
+ * @author Tanti
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Pelajar mahasiswa = new Pelajar("Tanti", 157, 60, "E41202195", 
+                "SMKN 7 Jember", 99.9);
+        System.out.println(mahasiswa.toString());
+    }
+    
+}
