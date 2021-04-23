d1270d14f0b45ace414911eb7e1f728b49b11b9b
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:58:13 +0700

    Create B.java

---
+++ Minggu 7/Latihan_Week7/Pewarisan/B.java
@@ -0,0 +1,14 @@
+package Pewarisan;
+
+/**
+ *
+ * @author Tanti
+ */
+public class B extends A {
+
+    int z;
+    
+    void TampilkanJumlah() {
+        System.out.println("Jumlah = " + (x+y+z));
+    }
+}
