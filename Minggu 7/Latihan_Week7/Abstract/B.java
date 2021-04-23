68c9f0fdeddc8ed837a49a1a8c9f72e5d44cf79c
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:55:57 +0700

    Create B.java

---
+++ Minggu 7/Latihan_Week7/Abstract/B.java
@@ -0,0 +1,12 @@
+package Abstract;
+
+/**
+ *
+ * @author Tanti
+ */
+public class B extends A{
+    public void method2() {
+        System.out.println("Method astract yang sudah mejadi konkrit dalam class B");
+                
+    }
+}
