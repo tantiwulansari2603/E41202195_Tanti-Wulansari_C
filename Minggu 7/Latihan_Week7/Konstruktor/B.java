306628dd178b1d0c9826e1a658a2d0df0a916462
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:56:21 +0700

    Create B.java

---
+++ Minggu 7/Latihan_Week7/Konstruktor/B.java
@@ -0,0 +1,12 @@
+package Konstruktor;
+
+/**
+ *
+ * @author Tanti
+ */
+public class B extends A{
+    B() {
+        super(" test");
+        System.out.println("Konstruktor class B dieksekusi ...");
+    }
+}
