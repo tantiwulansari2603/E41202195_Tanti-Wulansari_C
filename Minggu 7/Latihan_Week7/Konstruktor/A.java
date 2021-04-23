8b1ef55f57036a897b1cb547f1a1dcce86e9470e
tantiwulansari2603 <tantiwulansari2603@gmail.com>
2021-04-23 21:56:14 +0700

    Create A.java

---
+++ Minggu 7/Latihan_Week7/Konstruktor/A.java
@@ -0,0 +1,11 @@
+package Konstruktor;
+
+/**
+ *
+ * @author Tanti
+ */
+public class A {
+    A(String param1) {
+        System.out.println("Konstruktor class A dengan parameter " + param1 + " dieksekusi ...");
+    }
+}
